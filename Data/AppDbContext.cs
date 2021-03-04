29b5032ad3f26755ee9947adc69da0337e38fb15
guiluizmaia <68974994+guiluizmaia@users.noreply.github.com>
2021-03-04 00:02:07 -0300

    Add files via upload

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiProdutos.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Produto>()
+                .Property(p => p.Nome)
+                    .HasMaxLength(80);
+
+            modelBuilder.Entity<Produto>()
+                .Property(p => p.Preco)
+                    .HasPrecision(10, 2);
+
+            modelBuilder.Entity<Produto>()
+                .HasData(
+                    new Produto { Id = 1, Nome = "caderno", Preco= 7.95M, Estoque = 10}
+                );
+        }
+    }
+}
